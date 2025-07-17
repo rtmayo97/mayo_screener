5c480bad8ed72ecd9473d1f78fc8813256178405
rtmayo97 <r.tyler.mayo@gmail.com>
2025-07-17 16:35:55 -0500

    Update Stock_Screener.py

--- Stock_Screener.py
+++ Stock_Screener.py
@@ -279,8 +279,8 @@ if check_password():
 
                 st.subheader(f"{ticker} Snapshot")
                 st.write({
-                    'Price': current_price,
-                    'Percent Change': percent_change,
+                    'Price': f\"${current_price:.2f}\",
+                    'Percent Change': f\"{percent_change:.2f}%\",
                     'RVOL': rvol,
                     'ATR': atr,
                     'Benzinga News': benzinga_news,
